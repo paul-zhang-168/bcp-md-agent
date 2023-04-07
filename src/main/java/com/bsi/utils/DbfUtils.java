e0279140f15d2a60fe342bd1802328593bc841ab
fish <java_fish@126.com>
2023-04-07 16:20:04 +0800

    修改dbf插件

--- src/main/java/com/bsi/utils/DbfUtils.java
+++ src/main/java/com/bsi/utils/DbfUtils.java
@@ -1,83 +1,53 @@
 package com.bsi.utils;
 
 import com.bsi.framework.core.utils.ExceptionUtils;
+import com.linuxense.javadbf.DBFField;
+import com.linuxense.javadbf.DBFReader;
+import com.linuxense.javadbf.DBFRow;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 @Slf4j
 public class DbfUtils {
     private static Logger info_log = LoggerFactory.getLogger("TASK_INFO_LOG");
-    /**
-     * 读取指定路径的dbf文件数据
-     * @param path 文件路径
-     * @return  List<Map<String, Object>>
-     * @throws SQLException
-     */
-    public static List<Map<String, Object>> readDbf(String path,String sql,int skip)  {
-        List<Map<String, Object>> data = new ArrayList<>();
-        String url = "jdbc:dbf:" + path;
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
+
+    public static List<Map<String, Object>> readDbf(String filePath,String charSet,int skipRecords) {
+        File dbfFile = new File(filePath);
+        List<Map<String, Object>> resultList = new ArrayList<>();
+        DBFReader reader = null;
         try {
-            Class.forName("com.hxtt.sql.dbf.DBFDriver");
-            conn = DriverManager.getConnection(url);
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery(sql);
+            FileInputStream fis = new FileInputStream(dbfFile);
+            reader = new DBFReader(fis, Charset.forName(charSet));
+            reader.skipRecords(skipRecords);
+            DBFRow row;
             int rowNum = 0;
-            ResultSetMetaData metaData = rs.getMetaData();
-            int columnCount = metaData.getColumnCount();
-            while (rs.next()) {
-                rowNum++;
-                if(rowNum<=skip){
-                    continue;
+
+            while ((row = reader.nextRow()) != null) {
+                Map<String, Object> rowMap = new LinkedHashMap<>();
+                // 加上行号
+                rowMap.put("DBF_ROW_NO", ++rowNum);
+
+                for (int i = 0; i < reader.getFieldCount(); i++) {
+                    DBFField field = reader.getField(i);
+                    rowMap.put(field.getName(), row.getString(field.getName()));
                 }
-                Map<String, Object> row = new HashMap<>();
-                row.put("DBF_ROW_NO",rowNum);
-                for (int i = 1; i <= columnCount; i++) {
-                    String columnName = metaData.getColumnName(i);
-                    Object value = rs.getString(i);
-                    row.put(columnName, value);
-                }
-                data.add(row);
+                resultList.add(rowMap);
             }
         }catch (Exception e){
-            e.printStackTrace();
-            info_log.info("读取dbf文件报错,错误信息:{}", ExceptionUtils.getFullStackTrace(e));
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                    // ignore
-                }
-            }
-            if (stmt != null) {
-                try {
-                    stmt.close();
-                } catch (SQLException e) {
-                    // ignore
-                }
-            }
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                    // ignore
-                }
+            info_log.info("read DBF file error, msg:{}", ExceptionUtils.getFullStackTrace(e));
+        }finally {
+            if (reader!=null){
+                reader.close();
             }
         }
-        return data;
+        return resultList;
     }
 }
