299f3c57fa1daff3b7a1dde464a9d8d7ebdd51f4
fish <java_fish@126.com>
2023-05-08 15:02:19 +0800

    增加42号解析工具类

---
+++ src/main/java/com/bsi/utils/P42Utils.java
@@ -0,0 +1,49 @@
+package com.bsi.utils;
+
+import com.alibaba.fastjson.JSONObject;
+import com.bsi.framework.core.utils.ExceptionUtils;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 42号文工具类
+ */
+public class P42Utils {
+    private static Logger info_log = LoggerFactory.getLogger("TASK_INFO_LOG");
+    private static final String LINE_DELIMITER = "~";
+    private static final String FIELD_DELIMITER = ";";
+    private static final String EOF_MARKER = "||";
+
+    public static List<JSONObject> readData(String filePath) {
+
+        List<JSONObject> dataList = new ArrayList<>();
+        try {
+            List<String> lines= IOUtils.readLines(new BufferedReader(new FileReader(filePath)));
+            for(String line : lines) {
+                String[] ls = line.split(LINE_DELIMITER);
+                for (String l : ls) {
+                    if (l.trim().isEmpty() || l.equals(EOF_MARKER)) {
+                        continue;
+                    }
+                    String[] fields = l.split(FIELD_DELIMITER);
+                    JSONObject data = new JSONObject(true);
+                    int i = 0;
+                    for (String f : fields) {
+                        data.put(++i + "", f);
+                    }
+                    dataList.add(data);
+                }
+            }
+        } catch (IOException e) {
+            info_log.error(ExceptionUtils.getFullStackTrace(e));
+        }
+        return dataList;
+    }
+}
